3439ddde543ef1b5f181e8013e0ef5d1c3b9623d
mathias.ruckert <mathias.ruckert@kuehne-nagel.com>
2024-09-25 18:03:17 +0300

    2024-09-25 WIP: implementing authorization

--- authentication-flow/implicit-flow/src/App.tsx
+++ authentication-flow/implicit-flow/src/App.tsx
@@ -1,29 +1,32 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import { Logout } from "./Logout";
-import { Callback } from "./Callback";
-import { Login } from "./Login";
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {Logout} from "./Logout";
+import {Callback} from "./Callback";
+import {Login} from "./Login";
+import {AuthProvider} from "./AuthProvider.tsx";
 
 const router = createBrowserRouter([
-  {
-    path:"login",
-    element: <Login/>
-  },
-  {
-    path:"logout",
-    element: <Logout/>
-  },
-  {
-    path:"admin",
-    element: <div>Admin</div>,
-  },
-  {
-    path:"callback",
-    element: <Callback/>
-  }
+    {
+        path: "login",
+        element: <Login/>
+    },
+    {
+        path: "logout",
+        element: <Logout/>
+    },
+    {
+        path: "admin",
+        element: <div>Admin</div>,
+    },
+    {
+        path: "callback",
+        element: <Callback/>
+    }
 ]);
 
 function App() {
-  return <RouterProvider router={router}/>;
+    return <AuthProvider>
+        <RouterProvider router={router}/>
+    </AuthProvider>;
 }
 
 export default App
